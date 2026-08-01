e4a380e777f57554d417b764b9066f567d169b42
Digital-pintu <lpofficial2904@gmail.com>
2026-08-01 19:21:59 +0530

    Update admin panel

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter , HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/admin">
-    {/* <HashRouter> */}
-    
+  <BrowserRouter>
       <AuthProvider>
         <App />
         <Toaster position="top-right" richColors closeButton duration={3000} theme="dark" />
       </AuthProvider>
-      {/* </HashRouter> */}
   </BrowserRouter>
 );
